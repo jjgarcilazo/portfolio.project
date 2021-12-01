1ce4e0c832d49e4433b90a3cd547fb6c7464388f
Jesse Garcilazo <jjgarcilazo@gmail.com>
2021-12-01 15:56:43 -0800

    nav bar mix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,91 @@
 import React from 'react'
 import logo from "../Components/jesse.png"
-
-
-
+import { Link } from 'react-scroll'
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img className="logo" src={logo} alt= ""/></a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>     
-            </ul>
-            
-          </div>
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-dark fixed-top'>
+      <div className='container'>
+        <Link
+          smooth={true}
+          to='home'
+          // offset={-110}
+          className='nav-link'
+        >
+          <img className='logo' src={logo} alt='logo...' />
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <FontAwesomeIcon icon={faBars} style={{ color: '#fff' }} />
+        </button>
+
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item active'>
+              <Link smooth={true} to='home' className='nav-link'>
+                Home <span className='sr-only'>(current)</span>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link smooth={true} to='about' offset={-110} className='nav-link'>
+                about me
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                smooth={true}
+                to='skills'
+                offset={-110}
+                className='nav-link'
+              >
+                skills
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                smooth={true}
+                to='experience'
+                offset={-110}
+                className='nav-link'
+              >
+                experience
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                smooth={true}
+                to='portfolio'
+                offset={-110}
+                className='nav-link'
+              >
+                portfolio
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                smooth={true}
+                to='contact'
+                offset={-110}
+                className='nav-link'
+              >
+                contact me
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
