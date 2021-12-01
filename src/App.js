bd5ac20f919a377bbd8f508597d869772b9d1991
Jesse Garcilazo <jjgarcilazo@gmail.com>
2021-12-01 15:06:42 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
-    <h2> Portfolio ... </h2>
+    <Navbar/>
   );
 }
 
